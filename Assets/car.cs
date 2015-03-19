d007ccbba2dec7fcea1a0921e051602798b79bc5
xCi7er <xciter@hotmail.co.uk>
2015-03-19 22:22:45 +0000

    Added new prefabs, made a little town area, added a car that drives

---
+++ Assets/car.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class car : MonoBehaviour {
+
+	public WheelCollider wheelLeft;
+	public WheelCollider wheelRight;
+	public WheelCollider wheelBLeft;
+	public WheelCollider wheelBright;
+
+	public float speed = 15;
+	public float breaking = 25;
+	public float turning = 25;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		wheelBright.motorTorque = Input.GetAxis ("Vertical") * speed;
+		wheelBLeft.motorTorque = Input.GetAxis ("Vertical") * speed;
+
+		wheelBLeft.brakeTorque = 0;
+		wheelBright.brakeTorque = 0;
+
+		wheelLeft.steerAngle = Input.GetAxis ("Horizontal") * turning;
+		wheelRight.steerAngle = Input.GetAxis ("Horizontal") * turning;
+
+		if(Input.GetKey(KeyCode.Space))
+		{
+			wheelBLeft.brakeTorque = breaking;
+			wheelBright.brakeTorque = breaking;
+		}
+
+
+	}
+}
